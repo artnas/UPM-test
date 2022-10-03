34e961d4e6ba2dfa8673a7ebadb778632ea7aaf0
Artur Nasiadko <Arminator700@gmail.com>
2022-10-03 15:43:11 +0200

    Add files via upload

---
+++ Editor/MeshSplitControllerEditor.cs
@@ -0,0 +1,31 @@
+﻿/* https://github.com/artnas/Unity-Plane-Mesh-Splitter */
+
+using MeshSplit.Scripts;
+using UnityEditor;
+using UnityEngine;
+
+namespace MeshSplit.Editor
+{
+    [CustomEditor(typeof(MeshSplitController))]
+    public class MeshSplitControllerEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            MeshSplitController instance = (MeshSplitController)target;
+
+            DrawDefaultInspector();
+
+            if (GUILayout.Button("Create submeshes"))
+            {
+                Undo.RecordObject(instance, "Created submeshes");
+                instance.Split();
+            }
+
+            if (GUILayout.Button("Clear submeshes"))
+            {
+                Undo.RecordObject(instance, "Cleared submeshes");
+                instance.Clear();
+            }
+        }
+    }
+}
